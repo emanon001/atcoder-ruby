6f3e9065d8eecf93e624772c23fa8f7c872ebdeb
emanon001 <emanon001@gmail.com>
2019-06-22 07:30:55 +0900

    fix priority-queue

--- lib/priority_queue.rb
+++ lib/priority_queue.rb
@@ -1,23 +1,62 @@
 class PriorityQueue
+  include Enumerable
+
+  MIN = -(2 << 60)
+
+  attr_reader :size
+
   def initialize(&block)
     @size = 0
     @heap = Array.new
-    @block = block
+    @get_key = block || :itself.to_proc
   end
 
-  def insert(x)
+  def each
+    (1..@size).each do |i|
+      yield @heap[i]
+    end
+  end
+
+  def pop
+    return nil if @size <= 0
+    max = @heap[1]
+    @heap[1] = @heap[@size]
+    @size -= 1
+    max_heapify(1)
+    max
+  end
+
+  def push(x)
     @size += 1
-    i = @size - 1
-    @heap[i] = nil
-    increase_key(i, x)
+    i = @size
+    @heap[i] = x
+    while i > 1 && @get_key.call(@heap[i / 2]) < @get_key.call(@heap[i])
+      t = @heap[i / 2]
+      @heap[i / 2] = @heap[i]
+      @heap[i] = t
+      i = i / 2
+    end
+  end
+
+  def top
+    return nil if @size <= 0
+    @heap[1]
   end
 
   private
 
-  def increase_key(i, x)
-    key = @block.call(x)
-    fail 'key < @heap[i]' if key < @heap[i]
-    @heap[i] = x
-    # TODO:
+  def max_heapify(i)
+    l = 2 * i
+    r = 2 * i + 1
+    largest =
+      l <= @size && @get_key.call(@heap[l]) > @get_key.call(@heap[i]) ? l : i
+    largest = r \
+      if r <= @size && @get_key.call(@heap[r]) > @get_key.call(@heap[largest])
+    unless largest == i
+      t = @heap[i]
+      @heap[i] = @heap[largest]
+      @heap[largest] = t
+      max_heapify(largest)
+    end
   end
 end
---
+++ test/priority_queue_test.rb
@@ -0,0 +1,42 @@
+require 'test/unit'
+require '../lib/priority_queue'
+
+class PriorityQueueTest < Test::Unit::TestCase
+  def test_no_block
+    pq = PriorityQueue.new
+    pq.push(1)
+    pq.push(3)
+    pq.push(2)
+    assert_equal(pq.size, 3)
+    assert_equal(pq.top, 3)
+    assert_equal(pq.pop, 3)
+    assert_equal(pq.size, 2)
+    assert_equal(pq.top, 2)
+    assert_equal(pq.pop, 2)
+    assert_equal(pq.size, 1)
+    assert_equal(pq.top, 1)
+    assert_equal(pq.pop, 1)
+    assert_equal(pq.size, 0)
+    assert_equal(pq.top, nil)
+    assert_equal(pq.pop, nil)
+  end
+
+  def test_block_given
+    pq = PriorityQueue.new { |x| -x }
+    pq.push(1)
+    pq.push(3)
+    pq.push(2)
+    assert_equal(pq.size, 3)
+    assert_equal(pq.top, 1)
+    assert_equal(pq.pop, 1)
+    assert_equal(pq.size, 2)
+    assert_equal(pq.top, 2)
+    assert_equal(pq.pop, 2)
+    assert_equal(pq.size, 1)
+    assert_equal(pq.top, 3)
+    assert_equal(pq.pop, 3)
+    assert_equal(pq.size, 0)
+    assert_equal(pq.top, nil)
+    assert_equal(pq.pop, nil)
+  end
+end
