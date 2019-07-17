df8d708c5c59231a70608f4692ddddde9471854c
emanon001 <emanon001@gmail.com>
2019-07-18 03:12:21 +0900

    Priority-Queue がやけに遅いので dijkstra で使用しないよう修正

--- lib/dijkstra.rb
+++ lib/dijkstra.rb
@@ -1,15 +1,14 @@
 def dijkstra(s, graph)
-  pq = PriorityQueue.new { |(_, d)| -d }
   d_list = Array.new(graph.size, 1 << 60)
   d_list[s] = 0
-  pq.push([s, 0])
-  while !pq.empty?
-    v, d = pq.pop
+  queue = [[s, 0]]
+  while !queue.empty?
+    v, d = queue.shift
     next if d > d_list[v]
     graph[v].each do |(u, c)|
       if d_list[u] > d_list[v] + c
         d_list[u] = d_list[v] + c
-        pq.push([u, d_list[u]])
+        queue.push([u, d_list[u]])
       end
     end
   end
