5da56b081f703136b93a6facc77899115e2b12e7
emanon001 <emanon001@gmail.com>
2019-07-23 05:59:51 +0900

    fix abc134/e

--- abc134/e/main.rb
+++ abc134/e/main.rb
@@ -1,12 +1,22 @@
+def lower_bound(ary, x)
+  i = ary.bsearch_index { |y| x <= y }
+  i ? i : ary.size
+end
+
+def upper_bound(ary, x)
+  i = ary.bsearch_index { |y| x < y }
+  i ? i : ary.size
+end
+
 N = gets.to_i
 a_list = N.times.map { gets.to_i }
 list = []
 a_list.each do |a|
-  i = list.bsearch_index { |b| a > b }
-  if i
-    list[i] = a
+  i = lower_bound(list, a)
+  if i == 0
+    list.unshift(a)
   else
-    list.push(a)
+    list[i - 1] = a
   end
 end
 puts list.size
