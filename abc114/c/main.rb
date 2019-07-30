c569006001b1528bb50df239a75d6f10b5802d35
emanon001 <emanon001@gmail.com>
2019-07-31 04:10:30 +0900

    solve abc114/c

--- abc114/c/main.rb
+++ abc114/c/main.rb
@@ -1,19 +1,16 @@
 N = gets.to_i
 digits = N.to_s.size
 
-def dfs(digits_size, digits = [], &block)
-  if digits.size == digits_size
-    block.call(digits)
-    return
-  end
-  [7, 5, 3].each do |n|
-    dfs(digits_size, digits + [n], &block)
+def dfs(digits_size, digits = [])
+  return [digits] if digits.size == digits_size
+  [7, 5, 3].flat_map do |n|
+    dfs(digits_size, digits + [n])
   end
 end
 
 ans = 0
 (3..digits).each do |d|
-  dfs(d) do |list|
+  dfs(d).each do |list|
     is_ok = list.join('').to_i <= N &&
       list.count { |n| n == 7 } >= 1 &&
       list.count { |n| n == 5 } >= 1 &&
