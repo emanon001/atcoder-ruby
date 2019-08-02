6c7ef2431a4cd63e11c3e836261eacf0341728c5
emanon001 <emanon001@gmail.com>
2019-08-02 13:21:32 +0900

    abc128/d

--- abc128/d/main.rb
+++ abc128/d/main.rb
@@ -1,25 +1,20 @@
 N, K = gets.split.map(&:to_i)
 v_list = gets.split.map(&:to_i)
 
-comb_list = []
-(0..([N, K].min)).each do |a|
-  (0..([N, K].min - a)).each do |b|
-    max_drop_count = K - a - b
-    comb_list.push([a, b, max_drop_count])
+ans = -1
+(0..([N, K].min)).each do |lc|
+  (0..([N - lc, K - lc].min)).each do |rc|
+    sorted = (v_list.take(lc) + v_list.drop(v_list.size - rc)).sort
+    sum = 0
+    restc = K - lc - rc
+    sorted.each do |v|
+      if v < 0 && restc > 0
+        restc -= 1
+        next
+      end
+      sum += v
+    end
+    ans = sum if sum > ans
   end
-end
-
-ans = -(2 << 60)
-comb_list.each do |(a, b, drop_c)|
-  nums = []
-  nums = nums + v_list.take(a) if a > 0
-  nums = nums + v_list[-b..-1] if b > 0
-  nums.sort!
-  while drop_c > 0 && !nums.empty? && nums[0] < 0
-    nums.shift
-    drop_c -= 1
-  end
-  sum = nums.reduce(0, &:+)
-  ans = sum if sum > ans
 end
 puts ans
