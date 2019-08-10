cf9b5a6bffeee41165c016e57cbcddbfef1643a1
emanon001 <emanon001@gmail.com>
2019-08-10 23:23:48 +0900

    ModComb を追加

---
+++ lib/mod_comb.rb
@@ -0,0 +1,22 @@
+class ModComb
+  def initialize(max, mod)
+    @mod = mod
+    @fac = Array.new(max + 1)
+    @finv = Array.new(max + 1)
+    @inv = Array.new(max + 1)
+    @fac[0] = @fac[1] = 1
+    @finv[0] = @finv[1] = 1
+    @inv[1] = 1
+    (2..max).each do |i|
+      @fac[i] = @fac[i - 1] * i % @mod
+      @inv[i] = @mod - @inv[@mod % i] * (@mod / i) % @mod
+      @finv[i] = @finv[i - 1] * @inv[i] % @mod
+    end
+  end
+
+  def comb(n, k)
+    return 0 if n < k
+    return 0 if n < 0 || k < 0
+    @fac[n] * (@finv[k] * @finv[n - k] % @mod) % @mod
+  end
+end
