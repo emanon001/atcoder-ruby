a160b7468ede7bfd21ee8591c969ca7ff6d68ca5
emanon001 <emanon001@gmail.com>
2019-09-15 20:50:51 +0900

    solve abc132/d

--- abc132/d/main.rb
+++ abc132/d/main.rb
@@ -1,43 +1,32 @@
-def comb_init(max, mod)
-  fac = Array.new(max)
-  finv = Array.new(max)
-  inv = Array.new(max)
-  fac[0] = fac[1] = 1
-  finv[0] = finv[1] = 1
-  inv[1] = 1
-  (2...max).each do |i|
-    fac[i] = fac[i - 1] * i % mod
-    inv[i] = mod - inv[mod % i] * (mod / i) % mod
-    finv[i] = finv[i - 1] * inv[i] % mod
+class ModComb
+  def initialize(max, mod)
+    @mod = mod
+    @fac = Array.new(max + 1)
+    @finv = Array.new(max + 1)
+    @inv = Array.new(max + 1)
+    @fac[0] = @fac[1] = 1
+    @finv[0] = @finv[1] = 1
+    @inv[1] = 1
+    (2..max).each do |i|
+      @fac[i] = @fac[i - 1] * i % @mod
+      @inv[i] = @mod - @inv[@mod % i] * (@mod / i) % @mod
+      @finv[i] = @finv[i - 1] * @inv[i] % @mod
+    end
   end
-  { fac: fac, finv: finv, mod: mod }
-end
 
-def comb(n, k, context)
-  return 0 if n < k
-  return 0 if n < 0 || k < 0
-  fac = context[:fac]
-  finv = context[:finv]
-  mod = context[:mod]
-  fac[n] * (finv[k] * finv[n - k] % mod) % mod
+  def comb(n, k)
+    return 0 if n < k
+    return 0 if n < 0 || k < 0
+    @fac[n] * (@finv[k] * @finv[n - k] % @mod) % @mod
+  end
 end
-
 
 N, K = gets.split.map(&:to_i)
-red = N - K
 MOD = 10 ** 9 + 7
-ctx = comb_init(10 ** 7, MOD)
-K.times do |i|
-  blue = i + 1
-  if red + 1 == blue
-    puts 1
-    next
-  end
-  if red + 1 < blue
-    puts 0
-    next
-  end
-  n = red + blue + blue
-  c = comb(n, blue, ctx)
-  puts c
+blue = K
+red = N - K
+comb = ModComb.new(10 ** 5, MOD)
+(1..K).each do |k|
+  box = k
+  puts comb.comb(blue - 1, box - 1) * comb.comb(red + 1, box) % MOD
 end
