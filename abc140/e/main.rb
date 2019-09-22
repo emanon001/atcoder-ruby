97ccdb89c6d9aec303aa341c5295e8b3d17c8a5a
emanon001 <emanon001@gmail.com>
2019-09-23 03:42:40 +0900

    wip abc140/e

--- abc140/e/main.rb
+++ abc140/e/main.rb
@@ -1,12 +1,2 @@
 N = gets.to_i
-ps = gets.split.map(&:to_i).sort
-ans = 0
-n = N * (N - 1) / 2
-ans = n
-i = 0
-(N - 2 - 1).times do
-  n -= ps[i]
-  ans += n
-  i += 1
-end
-puts ans
+ps = gets.split.map(&:to_i)
