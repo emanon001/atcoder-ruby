2b58a89fd05d29184cad11cb71962a16531870e1
emanon001 <emanon001@gmail.com>
2019-10-15 03:57:23 +0900

    solve abc069/c

---
+++ abc069/c/main.rb
@@ -0,0 +1,19 @@
+N = gets.to_i
+as = gets.split.map(&:to_i)
+
+two_count = 0
+four_count = 0
+as.each do |a|
+  if a % 4 == 0
+    four_count += 1
+  elsif a % 2 == 0
+    two_count += 1
+  end
+end
+n = two_count
+m = if two_count > 0
+  four_count * 2
+else
+  four_count * 2 + 1
+end
+puts(n + m >= N ? 'Yes' : 'No')
