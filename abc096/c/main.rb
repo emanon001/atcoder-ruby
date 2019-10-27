8351f00ae43e5433653595aaabcfbeb63d905f6e
emanon001 <emanon001@gmail.com>
2019-10-27 14:14:04 +0900

    solve abc096/{a-c}

--- abc096/c/main.rb
+++ abc096/c/main.rb
@@ -1,13 +1,16 @@
 H, W = gets.split.map(&:to_i)
 board = H.times.map { gets.chomp.chars }
-is_ok = H.times.all? do |i|
-  W.times.all? do |j|
-    cell = board[i][j]
-    next true if cell == '.'
-    (i > 0 && board[i - 1][j] == '#') ||
-    (j < W - 1 && board[i][j + 1] == '#') ||
-    (i < H - 1 && board[i + 1][j] == '#') ||
-    (j > 0 && board[i][j - 1] == '#')
+H.times do |i|
+  W.times do |j|
+    next unless board[i][j] == '#'
+    is_ok = (i == 0 || board[i - 1][j] == '#') ||
+      (i == H - 1 || board[i + 1][j] == '#') ||
+      (j == 0 || board[i][j - 1] == '#') ||
+      (j == W - 1 || board[i][j + 1] == '#')
+    unless is_ok
+      puts 'No'
+      exit 0
+    end
   end
 end
-puts(is_ok ? 'Yes' : 'No')
+puts 'Yes'
