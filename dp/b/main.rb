02778aec2ed6dd0dfa3d1ab725d8e809dfc759af
emanon001 <emanon001@gmail.com>
2020-01-25 02:38:03 +0900

    update dp/b

--- dp/b/main.rb
+++ dp/b/main.rb
@@ -1,13 +1,16 @@
 N, K = gets.split.map(&:to_i)
-h_list = gets.split.map(&:to_i)
-INF = 2 << 60
-dp = Array.new(N + 10) { INF }
+hs = gets.split.map(&:to_i)
+
+INF = 1 << 60
+dp = Array.new(N + K, INF)
 dp[0] = 0
-(N - 1).times do |i|
-  (1..K).each do |k|
-    next if i + k >= N
-    cost = dp[i] + (h_list[i] - h_list[i + k]).abs
-    dp[i + k] = cost if cost < dp[i + k]
+N.times do |i|
+  n = dp[i]
+  (1..K).each do |j|
+    break if i + j >= N
+    m = dp[i + j]
+    cost = (hs[i] - hs[i + j]).abs
+    dp[i + j] = n + cost if n + cost < m
   end
 end
 puts dp[N - 1]
