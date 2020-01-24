57528db463afbcf499d221a05da05c4c406f205f
emanon001 <emanon001@gmail.com>
2020-01-25 02:46:54 +0900

    update dp/c

--- dp/c/main.rb
+++ dp/c/main.rb
@@ -1,21 +1,14 @@
 N = gets.to_i
-abc_list = N.times.map { gets.split.map(&:to_i) }
-dp = Array.new(N + 10)
-dp[0] = { a: 0, b: 0, c: 0 }
+abcs = N.times.map { gets.split.map(&:to_i) }
+
+dp = Array.new(N + 10) { Array.new(3, 0) }
 N.times do |i|
-  a, b, c = abc_list[i]
-  dp[i + 1] = {}
-  dp[i + 1][:a] = [
-    dp[i][:b] + a,
-    dp[i][:c] + a
-  ].max
-  dp[i + 1][:b] = [
-    dp[i][:a] + b,
-    dp[i][:c] + b
-  ].max
-  dp[i + 1][:c] = [
-    dp[i][:a] + c,
-    dp[i][:b] + c
-  ].max
+  a, b, c = abcs[i]
+  da, db, dc = dp[i]
+  dp[i + 1] = [
+    [db + a, dc + a].max,
+    [da + b, dc + b].max,
+    [da + c, db + c].max
+  ]
 end
-puts dp[N].values.max
+puts dp[N].max
