f787ca3a9c5a465a714c268a1b01ac1c250a4029
emanon001 <emanon001@gmail.com>
2020-01-25 17:51:58 +0900

    update abc129/c

--- abc129/c/main.rb
+++ abc129/c/main.rb
@@ -1,20 +1,17 @@
 require 'set'
-MOD = 10 ** 9 + 7
-N, M = gets.split.map(&:to_i)
-a_list = M.times.map { gets.to_i }
-a_set = Set.new(a_list)
 
-dp = Array.new(N + 1) { 0 }
+N, M = gets.split.map(&:to_i)
+A = Set.new(M.times.map { gets.to_i })
+MOD = 10 ** 9 + 7
+dp = Array.new(N + 10, 0)
 dp[0] = 1
-(0..(N - 1)).each do |i|
-  next if a_set.include?(i)
-  j = i + 1
-  if !a_set.include?(j)
-    dp[j] = (dp[j] + dp[i]) % MOD
+N.times do |i|
+  next if A.include?(i)
+  if i + 1 <= N && !A.include?(i + 1)
+    dp[i + 1] = (dp[i + 1] + dp[i]) % MOD
   end
-  j = i + 2
-  if j <= N && !a_set.include?(j)
-    dp[j] = (dp[j] + dp[i]) % MOD
+  if i + 2 <= N && !A.include?(i + 2)
+    dp[i + 2] = (dp[i + 2] + dp[i]) % MOD
   end
 end
 puts dp[N]
