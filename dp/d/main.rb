562dc2e15717c71ffa05c632699e318e1697af6c
emanon001 <emanon001@gmail.com>
2020-01-26 17:23:28 +0900

    update dp/d (配るDP & j が丁度その重さ)

--- dp/d/main.rb
+++ dp/d/main.rb
@@ -5,8 +5,10 @@ dp = Array.new(N + 1) { Array.new(W + 1, 0) }
 N.times do |i|
   w, v = wvs[i]
   (0..W).each do |j|
+    base_v = dp[i][j]
+    next if j != 0 && base_v == 0
     current_v = dp[i + 1][j]
-    new_v = dp[i][j]
+    new_v = base_v
     dp[i + 1][j] = new_v > current_v ? new_v : current_v
     if j + w <= W
       current_v = dp[i + 1][j + w]
@@ -16,4 +18,5 @@ N.times do |i|
     end
   end
 end
-puts dp[N][W]
+ans = dp[N].max
+puts ans
