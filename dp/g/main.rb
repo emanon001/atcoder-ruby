ce7cd38479e2857b25a24139d6dae691ff0451ba
emanon001 <emanon001@gmail.com>
2020-01-29 19:33:01 +0900

    update dp/g

--- dp/g/main.rb
+++ dp/g/main.rb
@@ -1,28 +1,33 @@
 N, M = gets.split.map(&:to_i)
 edge_list = M.times.map { gets.split.map(&:to_i) }
+indegree = Array.new(N, 0)
 graph = Array.new(N) { [] }
-indegree = Array.new(N) { 0 }
 edge_list.each do |(u, v)|
-  u -= 1; v -= 1
+  u -= 1
+  v -= 1
   graph[u].push(v)
-  indegree[v] += 1
+  indegree[v] += 1 
+end
+zero_vertex_list = []
+indegree.each.with_index do |c, i|
+  zero_vertex_list.push(i) if c == 0
 end
 
-dp = Array.new(N) { 0 }
-queue = []
-indegree.each.with_index do |c, v|
-  queue.push(v) if c == 0
-end
-while !queue.empty?
-  v = queue.shift
-  (graph[v]).each do |v2|
-    indegree[v2] -= 1
-    if indegree[v2] == 0
-      queue.push(v2)
-      dp[v2] = dp[v] + 1 if dp[v] + 1 > dp[v2]
+def next_edges(vertex_list, graph, indegree)
+  ret = []
+  vertex_list.each do |u|
+    graph[u].each do |v|
+      indegree[v] -= 1
+      ret.push(v) if indegree[v] == 0
     end
   end
+  ret
 end
 
-ans = dp.max
+ans = 0
+while !zero_vertex_list.empty?
+  ans += 1
+  zero_vertex_list = next_edges(zero_vertex_list, graph, indegree)
+end
+ans -= 1
 puts ans
