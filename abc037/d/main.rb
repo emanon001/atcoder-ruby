c54cc6388292321233a0e173a0fdfbeb8d40b191
emanon001 <emanon001@gmail.com>
2020-01-30 02:33:56 +0900

    update abc037/d

--- abc037/d/main.rb
+++ abc037/d/main.rb
@@ -2,26 +2,29 @@ H, W = gets.split.map(&:to_i)
 board = H.times.map { gets.split.map(&:to_i) }
 MOD = 10 ** 9 + 7
 
-MEMO = {}
-def dfs(board, i, j)
-  return MEMO[[i, j]] if MEMO[[i, j]]
-  dy = [-1, 0, 1, 0]
-  dx = [0, 1, 0, -1]
-  count = 1
-  4.times do |ii|
-    y = i + dy[ii]
-    x = j + dx[ii]
-    next unless y >= 0 && y < H && x >= 0 && x < W
-    next unless board[i][j] > board[y][x]
-    count += dfs(board, y, x) % MOD
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+DXY = dx.zip(dy).to_a
+
+def dfs(i, j, dp, board)
+  return dp[i][j] if dp[i][j]
+  a = board[i][j]
+  ret = 1
+  DXY.each do |dx, dy|
+    ii = i + dy
+    jj = j + dx
+    if ii >= 0 && ii < H && jj >= 0 && jj < W && board[ii][jj] > a
+      ret = (ret + dfs(i + dy, j + dx, dp, board)) % MOD
+    end
   end
-  MEMO[[i, j]] = count
+  dp[i][j] = ret
 end
 
+dp = Array.new(H) { Array.new(W, nil) }
 ans = 0
-(0...H).each do |i|
-  (0...W).each do |j|
-    ans += dfs(board, i, j) % MOD
+H.times do |i|
+  W.times do |j|
+    ans = (ans + dfs(i, j, dp, board)) % MOD
   end
 end
 puts ans
