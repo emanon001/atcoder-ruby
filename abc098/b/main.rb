c16acfd84e1a8173cd52ca9d72b2a7038d0e1915
emanon001 <emanon001@gmail.com>
2020-02-11 13:43:15 +0900

    update abc098/b

--- abc098/b/main.rb
+++ abc098/b/main.rb
@@ -1,9 +1,12 @@
-require 'set'
 N = gets.to_i
 S = gets.chomp.chars
+
+require 'set'
 ans = 0
-(1...N).each do |i|
-  size = (Set.new(S[0...i]) & Set.new(S[i..-1])).size
-  ans = size if size > ans
+(1..(N - 1)).each do |i|
+  a = Set.new(S[0...i])
+  b = Set.new(S[i..-1])
+  c = (a & b).size
+  ans = c if c > ans
 end
 puts ans
