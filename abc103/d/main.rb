e57a5e6ad9efc02f58a2fc59e9add843d7dea90c
emanon001 <emanon001@gmail.com>
2020-02-12 19:04:07 +0900

    update abc103/d

--- abc103/d/main.rb
+++ abc103/d/main.rb
@@ -1,25 +1,15 @@
 N, M = gets.split.map(&:to_i)
-abs = M.times.map { gets.split.map(&:to_i) }
-min_rs = Hash.new { 1 << 60 }
-abs.each do |(a, b)|
-  min_rs[a] = b if b < min_rs[a]
-end
+AB = M.times.map { gets.split.map(&:to_i) }
 
-ans = 0
-right = nil
-sorted_ls = min_rs.keys.sort
-sorted_ls.each do |l|
-  r = min_rs[l]
-  if right.nil?
-    right = r
-    next
-  end
-  if l >= right
-    ans += 1
-    right = r
-  else
-    right = r if r < right
+sorted = AB.sort_by { |a, b| [a, -b] }
+right = -(1 << 60)
+removed_count = 0
+sorted.each do |a, b|
+  if a >= right
+    removed_count += 1
+    right = b
+  elsif b < right
+    right = b
   end
 end
-ans += 1
-puts ans
+puts removed_count
