4079cb0ba62975e95fb7730f7f96211fefefe902
emanon001 <emanon001@gmail.com>
2020-02-12 23:57:21 +0900

    update tdpc/a

--- tdpc/a/main.rb
+++ tdpc/a/main.rb
@@ -2,17 +2,15 @@ N = gets.to_i
 P = gets.split.map(&:to_i)
 
 MAX_P = 100 * N
-dp = Array.new(N + 10) { Array.new(MAX_P + 10, false) }
-dp[0][0] = true
+dp = Array.new(N + 1) { Array.new(MAX_P + 1, 0) }
+dp[0][0] = 1
 N.times do |i|
   point = P[i]
   (0..MAX_P).each do |j|
-    current = dp[i][j]
-    dp[i + 1][j] ||= current
-    next unless current
-    next if j + point > MAX_P
-    dp[i + 1][j + point] = true
+    new_c = dp[i][j]
+    new_c += dp[i][j - point] if j - point >= 0
+    dp[i + 1][j] = new_c
   end
 end
-ans = dp[N].count(&:itself)
+ans = dp[N].count { |c| c > 0 }
 puts ans
