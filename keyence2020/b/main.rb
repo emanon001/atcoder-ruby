3df4d63df3167a1534e064df794dab15e5e63302
emanon001 <emanon001@gmail.com>
2020-02-12 17:54:18 +0900

    update keyence2020/b

--- keyence2020/b/main.rb
+++ keyence2020/b/main.rb
@@ -1,17 +1,17 @@
 N = gets.to_i
-xls = N.times.map { gets.split.map(&:to_i) }
+XL = N.times.map { gets.split.map(&:to_i) }
 
-sorted = xls.sort_by { |(x, l)| x - l }
+sorted = XL.sort_by { |x, l| x - l }
 ans = 0
-r = -(1 << 60)
-sorted.each do |(x, l)|
+current_right = -(1 << 60)
+sorted.each do |x, l|
   left = x - l
   right = x + l
-  if left >= r
+  if left >= current_right
     ans += 1
-    r = right
-  elsif right < r
-    r = right
+    current_right = right
+  elsif right < current_right
+    current_right = right
   end
 end
 puts ans
