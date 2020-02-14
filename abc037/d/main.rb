d4b8e497a3712a56c740505d1a99189e3d9b9286
emanon001 <emanon001@gmail.com>
2020-02-14 23:09:20 +0900

    update abc037/d

--- abc037/d/main.rb
+++ abc037/d/main.rb
@@ -1,20 +1,21 @@
 H, W = gets.split.map(&:to_i)
 board = H.times.map { gets.split.map(&:to_i) }
+
 MOD = 10 ** 9 + 7
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-DXY = dx.zip(dy).to_a
+DYX = [-1, 0, 1, 0].zip([0, 1, 0, -1]).to_a
 
 def dfs(i, j, dp, board)
   return dp[i][j] if dp[i][j]
-  a = board[i][j]
   ret = 1
-  DXY.each do |dx, dy|
+  from = board[i][j]
+  DYX.each do |dy, dx|
     ii = i + dy
     jj = j + dx
-    if ii >= 0 && ii < H && jj >= 0 && jj < W && board[ii][jj] > a
-      ret = (ret + dfs(i + dy, j + dx, dp, board)) % MOD
+    next if ii < 0 || ii >= H || jj < 0 || jj >= W
+    to = board[ii][jj]
+    if from > to
+      ret = (ret + dfs(ii, jj, dp, board)) % MOD
     end
   end
   dp[i][j] = ret
