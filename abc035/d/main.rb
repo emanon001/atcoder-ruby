98482011e9ee8ed91505640642a42156a787c900
emanon001 <emanon001@gmail.com>
2020-02-18 05:16:53 +0900

    update abc035/d

--- abc035/d/main.rb
+++ abc035/d/main.rb
@@ -1,38 +1,108 @@
-N, M, T = gets.split.map(&:to_i)
-a_list = gets.split.map(&:to_i)
-abc_list = M.times.map { gets.split.map(&:to_i) }
-graph1 = Array.new(N) { Array.new }
-graph2 = Array.new(N) { Array.new }
-abc_list.each do |(a, b, c)|
-  a -= 1
-  b -= 1
-  graph1[a].push([b, c])
-  graph2[b].push([a, c])
+class PriorityQueue
+  MIN = -(1 << 60)
+
+  attr_reader :size
+
+  def initialize(&block)
+    @size = 0
+    @heap = Array.new
+    @get_key = block || :itself.to_proc
+  end
+
+  def empty?
+    size == 0
+  end
+
+  def pop
+    return nil if @size <= 0
+    max = @heap[1]
+    @heap[1] = @heap[@size]
+    @size -= 1
+    max_heapify(1)
+    max
+  end
+
+  def push(x)
+    @size += 1
+    i = @size
+    @heap[i] = x
+    while i > 1 && (@get_key.call(@heap[i / 2]) <=> @get_key.call(@heap[i])) < 0
+      t = @heap[i / 2]
+      @heap[i / 2] = @heap[i]
+      @heap[i] = t
+      i = i / 2
+    end
+  end
+
+  def top
+    return nil if @size <= 0
+    @heap[1]
+  end
+
+  def to_a
+    return [] if @size == 0
+    @heap[1..@size]
+  end
+
+  private
+
+  def max_heapify(i)
+    l = 2 * i
+    r = 2 * i + 1
+    largest =
+      l <= @size && (@get_key.call(@heap[l]) <=> @get_key.call(@heap[i])) > 0 ? l : i
+    largest = r \
+      if r <= @size && (@get_key.call(@heap[r]) <=> @get_key.call(@heap[largest])) > 0
+    unless largest == i
+      t = @heap[i]
+      @heap[i] = @heap[largest]
+      @heap[largest] = t
+      max_heapify(largest)
+    end
+  end
 end
 
-def dijkstra(s, graph)
-  d_list = Array.new(graph.size, 1 << 60)
-  d_list[s] = 0
-  queue = [[s, 0]]
+N, M, T = gets.split.map(&:to_i)
+A = gets.split.map(&:to_i)
+
+a_graph = Array.new(N) { [] }
+b_graph = Array.new(N) { [] }
+M.times do
+  a, b, c = gets.split.map(&:to_i)
+  a -= 1
+  b -= 1
+  a_graph[a].push([b, c])
+  b_graph[b].push([a, c])
+end
+
+def shortest_path(graph, s)
+  w_list = Array.new(graph.size, 1 << 60)
+  w_list[s] = 0
+  queue = PriorityQueue.new do |_, c|
+    -c
+  end
+  queue.push([s, 0])
   while !queue.empty?
-    v, d = queue.shift
-    next if d > d_list[v]
-    graph[v].each do |(u, c)|
-      if d_list[u] > d_list[v] + c
-        d_list[u] = d_list[v] + c
-        queue.push([u, d_list[u]])
+    u, w = queue.pop
+    next if w > w_list[u]
+    graph[u].each do |v, c|
+      if w_list[u] + c < w_list[v]
+        new_cost = w_list[u] + c
+        w_list[v] = new_cost
+        queue.push([v, new_cost])
       end
     end
   end
-  d_list
+  w_list
 end
 
-d_list1 = dijkstra(0, graph1)
-d_list2 = dijkstra(0, graph2)
+a_cost = shortest_path(a_graph, 0)
+b_cost = shortest_path(b_graph, 0)
 ans = 0
-(0...N).each do |i|
-  rest = T - (d_list1[i] + d_list2[i])
-  n = rest * a_list[i]
-  ans = n if n > ans
+N.times do |i|
+  cost = a_cost[i] + b_cost[i]
+  next if cost >= T
+  point = A[i] * (T - cost)
+  ans = point if point > ans
 end
 puts ans
