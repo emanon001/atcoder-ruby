124701c8493ddf73e44f74c41ba5d98dc46950b2
emanon001 <emanon001@gmail.com>
2020-02-18 04:58:44 +0900

    update soundhound2018-summer-qual/d

--- lib/shortest_path.rb
+++ lib/shortest_path.rb
@@ -1,16 +1,38 @@
+# use PriorityQueue
 def shortest_path(graph, s)
   w_list = Array.new(graph.size, 1 << 60)
   w_list[s] = 0
-  queue = [[s, 0]]
+  queue = PriorityQueue.new do |_, c|
+    -c
+  end
+  queue.push([s, 0])
   while !queue.empty?
-    v, w = queue.shift
-    next if w > w_list[v]
-    graph[v].each do |(u, c)|
-      if w_list[u] > w_list[v] + c
-        w_list[u] = w_list[v] + c
-        queue.push([u, w_list[u]])
+    u, w = queue.pop
+    next if w > w_list[u]
+    graph[u].each do |v, c|
+      if w_list[u] + c < w_list[v]
+        new_cost = w_list[u] + c
+        w_list[v] = new_cost
+        queue.push([v, new_cost])
       end
     end
   end
   w_list
 end
+
+# def shortest_path(graph, s)
+#   w_list = Array.new(graph.size, 1 << 60)
+#   w_list[s] = 0
+#   queue = [[s, 0]]
+#   while !queue.empty?
+#     u, w = queue.shift
+#     next if w > w_list[u]
+#     graph[u].each do |(v, c)|
+#       if w_list[v] > w_list[u] + c
+#         w_list[v] = w_list[u] + c
+#         queue.push([v, w_list[v]])
+#       end
+#     end
+#   end
+#   w_list
+# end
--- soundhound2018-summer-qual/d/main.rb
+++ soundhound2018-summer-qual/d/main.rb
@@ -1,45 +1,45 @@
 N, M, S, T = gets.split.map(&:to_i)
-uvab_list = M.times.map { gets.split.map(&:to_i) }
-MONEY = 10 ** 15
-yen_graph = Array.new(N) { Array.new }
-snuke_graph = Array.new(N) { Array.new }
-uvab_list.each do |(u, v, a, b)|
-  u -= 1
-  v -= 1
+edges = M.times.map do
+  u, v, a, b = gets.split.map(&:to_i)
+  [u - 1, v - 1, a, b]
+end
+yen_graph = Array.new(N) { [] }
+snuuk_graph = Array.new(N) { [] }
+edges.each do |u, v, a, b|
   yen_graph[u].push([v, a])
   yen_graph[v].push([u, a])
-  snuke_graph[u].push([v, b])
-  snuke_graph[v].push([u, b])
+  snuuk_graph[u].push([v, b])
+  snuuk_graph[v].push([u, b])
 end
 
-def dijkstra(s, graph)
-  d_list = Array.new(graph.size, 1 << 60)
-  d_list[s] = 0
+def shortest_path(graph, s)
+  w_list = Array.new(graph.size, 1 << 60)
+  w_list[s] = 0
   queue = [[s, 0]]
   while !queue.empty?
-    v, d = queue.shift
-    next if d > d_list[v]
-    graph[v].each do |(u, c)|
-      if d_list[u] > d_list[v] + c
-        d_list[u] = d_list[v] + c
-        queue.push([u, d_list[u]])
+    u, w = queue.shift
+    next if w > w_list[u]
+    graph[u].each do |(v, c)|
+      if w_list[u] + c < w_list[v]
+        new_cost = w_list[u] + c
+        w_list[v] = new_cost
+        queue.push([v, new_cost])
       end
     end
   end
-  d_list
+  w_list
 end
 
-yen_cost_list = dijkstra(S - 1, yen_graph)
-snuke_cost_list = dijkstra(T - 1, snuke_graph)
-cost_list = yen_cost_list.zip(snuke_cost_list).map { |y, s| y + s }
-ans_list = Array.new(N)
-ans_list[N - 1] = MONEY - cost_list[N - 1]
-(N - 2).downto(0).each do |i|
-  n = MONEY - cost_list[i]
-  ans_list[i] = if n > ans_list[i + 1]
-    n
-  else
-    ans_list[i + 1]
-  end
+yen_cost = shortest_path(yen_graph, S - 1)
+snuuk_cost = shortest_path(snuuk_graph, T - 1)
+cost_list = N.times.map { |i| yen_cost[i] + snuuk_cost[i] }
+FIRST_YEN = 10 ** 15
+current_cost = 1 << 60
+ans = Array.new(N)
+(N - 1).downto(0) do |i|
+  current_cost = current_cost > cost_list[i] ? cost_list[i] : current_cost
+  ans[i] = FIRST_YEN - current_cost
 end
-ans_list.each { |ans| puts ans }
+ans.each do |a|
+  puts a
+end
