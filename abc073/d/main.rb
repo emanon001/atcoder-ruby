2e7e7d8a1378b32dd1d26e274e272aac5ab412b2
emanon001 <emanon001@gmail.com>
2020-02-20 05:03:43 +0900

    update abc073/d

--- abc073/d/main.rb
+++ abc073/d/main.rb
@@ -1,110 +1,30 @@
-class PriorityQueue
-  MIN = -(1 << 60)
-
-  attr_reader :size
-
-  def initialize(capacity = nil, &block)
-    @size = 0
-    @heap = if capacity.nil?
-      Array.new
-    else
-      # capacity には、queue のサイズとしてありえる最大の数を指定する想定。
-      # @heap[1] から使用するため、配列サイズを 1つ余分に確保する。
-      Array.new(capacity + 1)
-    end
-    @get_key = block || :itself.to_proc
-  end
-
-  def empty?
-    size == 0
-  end
-
-  def pop
-    return nil if @size <= 0
-    max = @heap[1]
-    @heap[1] = @heap[@size]
-    @size -= 1
-    max_heapify(1)
-    max
-  end
-
-  def push(x)
-    @size += 1
-    i = @size
-    @heap[i] = x
-    while i > 1 && (@get_key.call(@heap[i / 2]) <=> @get_key.call(@heap[i])) < 0
-      t = @heap[i / 2]
-      @heap[i / 2] = @heap[i]
-      @heap[i] = t
-      i = i / 2
-    end
-  end
-
-  def top
-    return nil if @size <= 0
-    @heap[1]
-  end
-
-  def to_a
-    return [] if @size == 0
-    @heap[1..@size]
-  end
-
-  private
-
-  def max_heapify(i)
-    l = 2 * i
-    r = 2 * i + 1
-    largest =
-      l <= @size && (@get_key.call(@heap[l]) <=> @get_key.call(@heap[i])) > 0 ? l : i
-    largest = r \
-      if r <= @size && (@get_key.call(@heap[r]) <=> @get_key.call(@heap[largest])) > 0
-    unless largest == i
-      t = @heap[i]
-      @heap[i] = @heap[largest]
-      @heap[largest] = t
-      max_heapify(largest)
-    end
-  end
-end
-
 N, M, R = gets.split.map(&:to_i)
 r_list = gets.split.map { |a| a.to_i - 1}
-graph = Array.new(N) { [] }
+INF = 1 << 60
+d = Array.new(N) { Array.new(N, INF) }
+N.times { |i| d[i][i] = 0 }
 M.times do
   u, v, c = gets.split.map(&:to_i)
   u -= 1
   v -= 1
-  graph[u].push([v, c])
-  graph[v].push([u, c])
+  d[u][v] = c
+  d[v][u] = c
 end
 
-def shortest_path(graph, s)
-  w_list = Array.new(graph.size, 1 << 60)
-  w_list[s] = 0
-  queue = PriorityQueue.new(graph.size) do |_, c|
-    -c
-  end
-  queue.push([s, 0])
-  while !queue.empty?
-    u, w = queue.pop
-    next if w > w_list[u]
-    graph[u].each do |v, c|
-      if w_list[u] + c < w_list[v]
-        new_cost = w_list[u] + c
-        w_list[v] = new_cost
-        queue.push([v, new_cost])
+def warshal_floyd(d)
+  v = d.size
+  v.times do |k|
+    v.times do |i|
+      v.times do |j|
+        cost = d[i][j]
+        new_cost = d[i][k] + d[k][j]
+        d[i][j] = new_cost if new_cost < cost 
       end
     end
   end
-  w_list
 end
 
-d = {}
-r_list.map do |r|
-  d[r] = shortest_path(graph, r)
-end
-
+warshal_floyd(d)
 ans = 1 << 60
 r_list.permutation.each do |l|
   ret = 0
