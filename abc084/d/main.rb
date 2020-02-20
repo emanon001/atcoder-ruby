41bebf6ec52c0cc718dbcd2ec73817ed7757d2c5
emanon001 <emanon001@gmail.com>
2020-02-20 10:28:33 +0900

    update abc084/d

--- abc084/d/main.rb
+++ abc084/d/main.rb
@@ -1,23 +1,17 @@
-require 'prime'
-require 'set'
 Q = gets.to_i
 queries = Q.times.map { gets.split.map(&:to_i) }
 
+require 'prime'
+require 'set'
 MAX = 10 ** 5
 primes = Set.new(Prime.each(MAX).to_a)
-
-list = Array.new(MAX + 1)
-count = 0
-list[0] = count
+cusum = Array.new(MAX + 1)
+cusum[0] = 0
 (1..MAX).each do |n|
-  if n.even?
-    list[n] = count
-    next
-  end
-  count += 1 if primes.include?(n) && primes.include?((n + 1) / 2)
-  list[n] = count
+  c = cusum[n - 1]
+  c += 1 if primes.include?(n) && n.odd? && primes.include?((n + 1) / 2)
+  cusum[n] = c
 end
-
 queries.each do |l, r|
-  puts list[r] - list[l - 1]
+  puts(cusum[r] - cusum[l - 1])
 end
