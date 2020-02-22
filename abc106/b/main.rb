b62d55b96592d81da4fb2a557284409a47b15ca4
emanon001 <emanon001@gmail.com>
2020-02-23 08:07:03 +0900

    update abc106/b

--- abc106/b/main.rb
+++ abc106/b/main.rb
@@ -1,11 +1,16 @@
 N = gets.to_i
-ans = 0
-(1..N).each do |n|
-  next if n.even?
-  count = 0
-  (1..n).each do |m|
-    count += 1 if n % m == 0
+
+def divisor_count(n)
+  ret = 0
+  (1..(Math.sqrt(n))).each do |m|
+    if n % m == 0
+      ret += (n / m == m ? 1 : 2)
+    end
   end
-  ans += 1 if count == 8
+  ret
+end
+
+ans = (1..N).count do |n|
+  n.odd? && divisor_count(n) == 8
 end
 puts ans
