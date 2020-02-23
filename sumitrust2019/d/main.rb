1f4ef238c9e21c599dce9d7437d6a29bc79f1ade
emanon001 <emanon001@gmail.com>
2020-02-23 23:38:30 +0900

    update sumitrust2019/d

--- sumitrust2019/d/main.rb
+++ sumitrust2019/d/main.rb
@@ -2,26 +2,21 @@ require 'set'
 
 N = gets.to_i
 S = gets.chomp.chars.map(&:to_i)
-idx = Hash.new
-S.each.with_index do |n, i|
-  idx[n] ||= []
-  idx[n].push(i)
+
+idx = Array.new(10) { [] }
+S.each.with_index do |ch, i|
+  idx[ch].push(i)
 end
 
 ans = 0
 (0..999).each do |n|
-  n1, n2, n3 = n.to_s.rjust(3, '0').chars.map(&:to_i)
-  idx1 = idx[n1]
-  next unless idx1
-  i1 = idx1[0]
-  idx2 = idx[n2]
-  next unless idx2
-  i2 = idx2.bsearch { |i2| i2 > i1 }
-  next unless i2
-  idx3 = idx[n3]
-  next unless idx3
-  i3 = idx3.bsearch { |i3| i3 > i2 }
-  next unless i3
-  ans += 1
+  i = -1
+  is_ok = n.to_s.rjust(3, '0').chars.all? do |ch|
+    i = idx[ch.to_i].bsearch do |x|
+      x > i
+    end
+    i != nil
+  end
+  ans += 1 if is_ok
 end
 puts ans
