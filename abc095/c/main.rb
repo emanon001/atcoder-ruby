ad51f11abac1f4080a7f65eda88b68132f5aa8b8
emanon001 <emanon001@gmail.com>
2020-02-23 23:22:02 +0900

    update abc095/c

--- abc095/c/main.rb
+++ abc095/c/main.rb
@@ -1,35 +1,10 @@
 A, B, C, X, Y = gets.split.map(&:to_i)
 
-x = 0
-y = 0
-ans = 0
-while x < X || y < Y
-  if x < X && y < Y
-    if A + B > C * 2
-      x += 1
-      y += 1
-      ans += C * 2
-    else
-      x += 1
-      y += 1
-      ans += A + B
-    end
-  elsif x < X
-    if A > C * 2
-      x += 1
-      ans += C * 2
-    else
-      x += 1
-      ans += A
-    end
-  elsif y < Y
-    if B > C * 2
-      y += 1
-      ans += C * 2
-    else
-      y += 1
-      ans += B
-    end
-  end
+ans = 1 << 60
+(0..([X, Y].max * 2)).each do |c|
+  sum = c * C
+  sum += A * ([X - c / 2, 0].max)
+  sum += B * ([Y - c / 2, 0].max)
+  ans = sum if sum < ans
 end
 puts ans
