ea337818959a0d358f183b866859b6a8405742b7
emanon001 <emanon001@gmail.com>
2020-02-25 11:47:18 +0900

    update abc145/c

--- abc145/c/main.rb
+++ abc145/c/main.rb
@@ -1,23 +1,10 @@
 N = gets.to_i
-xys = N.times.map { gets.split.map(&:to_i) }
-
-def dfs(cand, pos, sum)
-  return sum if cand.empty?
-  x1, y1 = pos
-  cand.reduce(0) do |acc, pos2|
-    tmp = cand.dup
-    tmp.delete(pos2)
-    x2, y2 = pos2
-    d = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-    acc += dfs(tmp, pos2, sum + d)
+XY = N.times.map { gets.split.map(&:to_i) }
+sum = 0.0
+(0...N).to_a.permutation.each do |path|
+  sum += path.each_cons(2).reduce(0) do |acc, (u, v)|
+    acc + Math.sqrt((XY[u][0] - XY[v][0]) ** 2 + (XY[u][1] - XY[v][1]) ** 2)
   end
-end
-
-sum = 0.to_f
-xys.each do |pos|
-  tmp = xys.dup
-  tmp.delete(pos)
-  sum += dfs(tmp, pos, 0)
 end
 ans = sum / (1..N).reduce(:*)
 puts ans
