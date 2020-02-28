f5d6c7339e0c2b7f6ba591eb3e0bd58b4621d5a2
emanon001 <emanon001@gmail.com>
2020-02-28 17:08:23 +0900

    update abc138/d

--- abc138/d/main.rb
+++ abc138/d/main.rb
@@ -1,34 +1,35 @@
 N, Q = gets.split.map(&:to_i)
-edges = (N - 1).times.map { gets.split.map(&:to_i) }
-queries = Q.times.map { gets.split.map(&:to_i) }
-
-graph = Array.new(N) { Array.new }
-edges.each do |(u, v)|
+graph = Array.new(N) { [] }
+(N - 1).times do
+  u, v = gets.split.map(&:to_i)
   u -= 1
   v -= 1
   graph[u].push(v)
   graph[v].push(u)
 end
-
-points = Array.new(N, 0)
-queries.each do |u, x|
-  points[u - 1] += x
+counters = Array.new(N, 0)
+Q.times do
+  u, x = gets.split.map(&:to_i)
+  u -= 1
+  counters[u] += x
 end
 
-def bfs(graph, s, points)
-  ret = Array.new(N, 0)
-  queue = [[s, -1, 0]]
+def bfs(graph, counters)
+  res = Array.new(N, 0)
+  res[0] = counters[0]
+  queue = [[0, nil]]
   while !queue.empty?
-    u, parent, parent_point = queue.shift
-    ret[u] = points[u] + parent_point
+    u, parent = queue.shift
+    c = res[u]
     graph[u].each do |v|
       next if v == parent
-      queue.push([v, u, ret[u]])
+      new_c = c + counters[v]
+      res[v] = new_c
+      queue.push([v, u])
     end
   end
-  ret
+  res
 end
 
-counters = bfs(graph, 0, points)
-ans = counters.join(' ')
-puts ans
+res = bfs(graph, counters)
+puts res.join(' ')
