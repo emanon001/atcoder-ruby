b4769d99c3d24fc9bf308fe893b59ecf081d4674
emanon001 <emanon001@gmail.com>
2020-02-29 22:57:32 +0900

    update abc088/d

--- abc088/d/main.rb
+++ abc088/d/main.rb
@@ -1,42 +1,37 @@
 H, W = gets.split.map(&:to_i)
-board = H.times.map { gets.chomp.chars }
-w_count = board.map do |row|
-  row.count('.')
-end.reduce(&:+)
+grid = H.times.map { gets.chomp.chars }
 
-INF = 1 << 30
-def bfs(board)
-  cost = Array.new(H) do
-    Array.new(W, INF)
+w_count = 0
+H.times do |i|
+  W.times do |j|
+    w_count += 1 if grid[i][j] == '.'
   end
-  cost[0][0] = 1
-  ys = [-1, 0, 1]
-  xs = [-1, 0, 1]
-  queue = [[0, 0, 1]]
+end
+
+def steps(grid, si, sj)
+  visited = Array.new(H) { Array.new(W, false) }
+  visited[si][sj] = true
+  dy = [-1, 0, 1, 0]
+  dx = [0, 1, 0, -1]
+  queue = [[si, sj, 1]]
   while !queue.empty?
-    i, j, c = queue.shift
-    ys.each do |y|
-      ii = i + y
-      next if ii < 0 || ii >= H
-      xs.each do |x|
-        next if y != 0 && x != 0
-        jj = j + x
-        next if jj < 0 || jj >= W
-        next if board[ii][jj] == '#'
-        if c + 1 < cost[ii][jj]
-          cost[ii][jj] = c + 1
-          queue.push([ii, jj, c + 1])
-        end
+    i, j, s = queue.shift
+    4.times do |d|
+      y = i + dy[d]
+      x = j + dx[d]
+      next if y < 0 || y >= H || x < 0 || x >= W
+      next if grid[y][x] == '#'
+      next if visited[y][x]
+      visited[y][x] = true
+      if y == H - 1 && x == W - 1
+        return s + 1
       end
+      queue.push([y, x, s + 1])
     end
   end
-  cost[H - 1][W - 1]
+  nil
 end
 
-cost = bfs(board)
-if cost == INF
-  puts -1
-  exit 0
-end
-ans = w_count - cost
+c = steps(grid, 0, 0)
+ans = c ? w_count - c : -1
 puts ans
