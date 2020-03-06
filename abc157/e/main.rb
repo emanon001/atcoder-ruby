90b439e99380823e110d33b7b3757f9f9da537af
emanon001 <emanon001@gmail.com>
2020-03-06 14:26:55 +0900

    change `SegmentTree.new` argument names

--- abc157/e/main.rb
+++ abc157/e/main.rb
@@ -1,13 +1,13 @@
 class SegmentTree
-  def initialize(size, default:, value: nil, update: -> (nv, _) { nv }, merge:)
+  def initialize(size, default:, value: nil, update: nil, merge:)
     n = 1
     while n < size
       n *= 2
     end
     @size = n
     @default = default
-    @update_proc = update
-    @merge_proc = merge
+    @update = update || -> (nv, _) { nv }
+    @merge = merge
     @data = Array.new(n * 2 - 1, default)
     init_by_value(value) if value
   end
@@ -15,10 +15,10 @@ class SegmentTree
   # 0-origin
   def update(k, a)
     k += @size - 1
-    @data[k] = @update_proc.call(a, @data[k])
+    @data[k] = @update.call(a, @data[k])
     while k > 0
       k = (k - 1) / 2
-      @data[k] = @merge_proc.call(@data[k * 2 + 1], @data[k * 2 + 2])
+      @data[k] = @merge.call(@data[k * 2 + 1], @data[k * 2 + 2])
     end
     nil
   end
@@ -36,7 +36,7 @@ class SegmentTree
     return @data[k] if a <= l && r <= b
     vl = execute_query(a, b, k * 2 + 1, l, (l + r) / 2)
     vr = execute_query(a, b, k * 2 + 2, (l + r) / 2, r)
-    @merge_proc.call(vl, vr)
+    @merge.call(vl, vr)
   end
 
   def init_by_value(value)
@@ -44,7 +44,7 @@ class SegmentTree
       @data[i + @size - 1] = v
     end
     (@size - 2).downto(0) do |i|
-      @data[i] = @merge_proc.call(@data[i * 2 + 1], @data[i * 2 + 2])
+      @data[i] = @merge.call(@data[i * 2 + 1], @data[i * 2 + 2])
     end
   end
 end
