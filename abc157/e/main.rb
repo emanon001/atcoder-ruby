6435da1f2286d539fe717d881f2dd41a17080bf9
emanon001 <emanon001@gmail.com>
2020-03-06 14:05:57 +0900

    solve abc157/e

--- abc157/e/main.rb
+++ abc157/e/main.rb
@@ -1,36 +1,63 @@
-class Bit
-  def initialize(n)
-    @bit = Array.new(n + 1, 0)
-    @n = n
+class SegmentTree
+  def initialize(size, default:, value: nil, update: -> (nv, _) { nv }, merge:)
+    n = 1
+    while n < size
+      n *= 2
+    end
+    @size = n
+    @default = default
+    @update_proc = update
+    @merge_proc = merge
+    @data = Array.new(n * 2 - 1, default)
+    init_by_value(value) if value
   end
 
   # 0-origin
-  def add(i, x)
-    i += 1
-    while i <= @n
-      @bit[i] += x
-      i += i & -i
+  def update(k, a)
+    k += @size - 1
+    @data[k] = @update_proc.call(a, @data[k])
+    while k > 0
+      k = (k - 1) / 2
+      @data[k] = @merge_proc.call(@data[k * 2 + 1], @data[k * 2 + 2])
     end
+    nil
   end
 
+  # [a, b)
   # 0-origin
-  def sum(i)
-    i += 1
-    s = 0
-    while i > 0
-      s += @bit[i]
-      i -= i & -i
+  def query(a, b)
+    execute_query(a, b, 0, 0, @size)
+  end
+
+  private
+
+  def execute_query(a, b, k, l, r)
+    return @default if r <= a || b <= l
+    return @data[k] if a <= l && r <= b
+    vl = execute_query(a, b, k * 2 + 1, l, (l + r) / 2)
+    vr = execute_query(a, b, k * 2 + 2, (l + r) / 2, r)
+    @merge_proc.call(vl, vr)
+  end
+
+  def init_by_value(value)
+    value.each.with_index do |v, i|
+      @data[i + @size - 1] = v
     end
-    s
+    (@size - 2).downto(0) do |i|
+      @data[i] = @merge_proc.call(@data[i * 2 + 1], @data[i * 2 + 2])
+    end
   end
 end
 
 N = gets.to_i
 S = gets.chomp.chars.map { |ch| ch.ord - 97 }
-bits = Array.new(26) { Bit.new(N) }
+st_value = Array.new(N, 0)
 S.each.with_index do |o, i|
-  bits[o].add(i, 1)
+  st_value[i] = 1 << o
 end
+st = SegmentTree.new(N, default: 0, value: st_value, merge: lambda do |a, b|
+  a | b
+end)
 Q = gets.to_i
 Q.times do
   k, a, b = gets.chomp.split
@@ -38,16 +65,13 @@ Q.times do
   k = k.to_i
   if k == 1
     b = b.ord - 97
-    next if S[a] == b
-    prev = S[a]
-    S[a] = b
-    bits[prev].add(a, -1)
-    bits[b].add(a, 1)
+    st.update(a, 1 << b)
   else
-    b = b.to_i - 1
+    b = b.to_i
+    x = st.query(a, b)
     ans = 0
     26.times do |i|
-      ans += 1 if bits[i].sum(b) - (a == 0 ? 0 : bits[i].sum(a - 1)) > 0
+      ans += 1 if x[i] == 1
     end
     puts ans
   end
