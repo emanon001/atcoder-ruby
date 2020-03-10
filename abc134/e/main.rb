5cea0276d1ed1549019ea0183abc25d7d0a81bee
emanon001 <emanon001@gmail.com>
2020-03-10 16:19:33 +0900

    update abc134/e

--- abc134/e/main.rb
+++ abc134/e/main.rb
@@ -1,22 +1,27 @@
-def lower_bound(ary, x)
-  i = ary.bsearch_index { |y| x <= y }
-  i ? i : ary.size
-end
-
-def upper_bound(ary, x)
-  i = ary.bsearch_index { |y| x < y }
-  i ? i : ary.size
-end
-
 N = gets.to_i
-a_list = N.times.map { gets.to_i }
+A = N.times.map { gets.to_i }
+
+def bsearch(ng:, ok:, &is_ok)
+  while (ok - ng).abs > 1
+    mid = (ok + ng) / 2
+    if is_ok.call(mid)
+      ok = mid
+    else
+      ng = mid
+    end
+  end
+  ok
+end
+
 list = []
-a_list.each do |a|
-  i = lower_bound(list, a)
-  if i == 0
+A.each do |a|
+  i = bsearch(ok: -1, ng: list.size) do |j|
+    list[j] < a
+  end
+  if i == -1
     list.unshift(a)
   else
-    list[i - 1] = a
+    list[i] = a
   end
 end
 puts list.size
