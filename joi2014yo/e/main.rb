c9189ea3b579ac5c5f1878013b52987f096b310d
emanon001 <emanon001@gmail.com>
2020-03-12 16:28:42 +0900

    delete unused dir

--- joi2014yo/e/main.rb
+++ joi2014yo/e/main.rb
@@ -1,3 +1,67 @@
+class PriorityQueue
+  attr_reader :size
+
+  def initialize(capacity = nil, &block)
+    @size = 0
+    @heap = capacity.nil? ? Array.new : Array.new(capacity)
+    @key = block || :itself.to_proc
+  end
+
+  def empty?
+    size == 0
+  end
+
+  def push(x)
+    i = @size
+    @size += 1
+    x_key = @key.call(x)
+    while i > 0
+      parent = (i - 1) / 2
+      break if (@key.call(@heap[parent]) <=> x_key) >= 0
+      @heap[i] = @heap[parent]
+      i = parent
+    end
+    @heap[i] = x
+    nil
+  end
+
+  def pop
+    return nil if @size <= 0
+    ret = @heap[0]
+    x = @heap[@size - 1]
+    x_key = @key.call(x)
+    @size -= 1
+    i = 0
+    while i * 2 + 1 < @size
+      l = i * 2 + 1
+      r = i * 2 + 2
+      largest = l
+      largest_key = @key.call(@heap[l])
+      if r < @size
+        r_key = @key.call(@heap[r])
+        if (r_key <=> largest_key) > 0
+          largest = r
+          largest_key = r_key 
+        end
+      end
+      break if (x_key <=> largest_key) >= 0
+      @heap[i] = @heap[largest]
+      i = largest
+    end
+    @heap[i] = x
+    ret
+  end
+
+  def top
+    return nil if @size <= 0
+    @heap[0]
+  end
+
+  def to_a
+    @heap.take(@size)
+  end
+end
+
 N, K = gets.split.map(&:to_i)
 CR = N.times.map { gets.split.map(&:to_i) }
 AB = K.times.map do
@@ -21,7 +85,7 @@ def reachable_vertexes(graph, s, max_c)
       new_c = c + 1
       next if new_c > max_c
       set.add(v)
-      queue.push([v, new_c]) if new_c <= max_c
+      queue.push([v, new_c])
     end
   end
   set
@@ -34,9 +98,12 @@ end
 def shortest_path(reachable, s)
   w_list = Array.new(reachable.size, 1 << 60)
   w_list[s] = 0
-  queue = [[s, 0]]
+  queue = PriorityQueue.new do |_, c|
+    -c
+  end
+  queue.push([s, 0])
   while !queue.empty?
-    u, w = queue.shift
+    u, w = queue.pop
     next if w > w_list[u]
     c = CR[u][0]
     reachable[u].each do |v|
